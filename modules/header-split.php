1968c69dab41388812ec28d41f7e8a38b0a80682
Sławek Amielucha <amielucha@gmail.com>
2016-06-29 19:03:38 +0200

    Updated the split header module

--- modules/header-split.php
+++ modules/header-split.php
@@ -3,7 +3,7 @@
 ?>
 	<header class="site-header">
 		<div class="site-header-inner">
-			<div class="container">
+			<div id="site-navigation-wrapper" class="site-navigation-wrapper container">
 				<nav id="site-navigation" class="main-navigation navbar" role="navigation">
 					<div class="navbar-header">
 						<div class="site-branding navbar-brand" itemscope itemtype="https://schema.org/logo">
@@ -12,13 +12,17 @@
 							<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
 						</div><!-- .site-branding -->
 					</div>
+					<label for="menu-toggle" class="menu-toggle-label hidden-md-up pull-right"><span class="hamburger" aria-hidden="true">&#8801;</span> Main Menu</label>
+			<input type="checkbox" id="menu-toggle" class="menu-toggle invisible" aria-controls="primary-menu" />
 					<?php if ( has_nav_menu( 'primary' ) ) wp_nav_menu (
-						array (
-							'theme_location' => 'primary',
-							'menu_class'     => 'nav navbar-nav pull-right',
-							'walker'				 => new iSeek_Menu_Walker()
-							)
-					) ?>
+				array (
+					'container_class'=> 'nav-menu-container',
+					'theme_location' => 'primary',
+					'menu_class'     => 'nav navbar-nav pull-right',
+					'menu_id'		 => 'primary-menu',
+					'walker'				 => new iSeek_Menu_Walker()
+					)
+			) ?>
 				</nav><!-- #site-navigation -->
 			</div>
 		</div>
